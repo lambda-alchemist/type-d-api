29df960da06659a2fcdace07c4dca93881caac3e
Guilherme M. Miranda <alchemist.software@proton.me>
2023-01-25 18:23:27 -0300

    changes

--- core/router/url.ts
+++ core/router/url.ts
@@ -1,14 +1,11 @@
 import { Context, Router } from "land:oak";
-import { root_api, root_rest } from "router/view.ts"
+import { root_api, root_crud, root_auth } from "router/view.ts"
 
 const router = new Router;
 
 router
 	.get("/",     (context: Context) => root_api(context))
-	.get("/rest", (context: Context) => root_rest(context))
+	.get("/rest", (context: Context) => root_crud(context))
+	.get("/auth", (context: Context) => root_auth(context))
 
-	// .get("/rest/user/", (context: Context) => crud_user.list(context))
-
-
-
-export { router };
+export default router;
--- core/router/view.ts
+++ core/router/view.ts
@@ -1,15 +1,14 @@
 import { Context } from "land:oak";
-import json_api from "router/url.json" assert { type: 'json' };
+import json_api  from "router/root.json" assert { type: 'json' };
+import json_crud from "router/crud.json" assert { type: 'json' };
+import json_auth from "router/auth.json" assert { type: 'json' };
 
-function root_api(context: Context): void {
-	context.response.body = "Welcome to the type-d-api!";
-}
-
-function root_rest(context: Context): void {
-	context.response.body = JSON.stringify(json_api);
-}
+function  root_api(context: Context): void { context.response.body = json_api; }
+function root_crud(context: Context): void { context.response.body = json_crud; }
+function root_auth(context: Context): void { context.response.body = json_auth; }
 
 export {
 	root_api,
-	root_rest,
-}
+	root_crud,
+	root_auth
+};
