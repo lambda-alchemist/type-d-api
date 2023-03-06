b56cefe1dc66154f62ada95636f604ccaf70d6c1
Guilherme M. Miranda <alchemist.software@proton.me>
2023-03-06 14:45:03 -0300

    changes

--- core/router.ts
+++ core/router.ts
@@ -3,7 +3,8 @@ import * as Crud    from "app:crud";
 import * as Auth    from "app:auth";
 
 const app  = new Oak.Router();
-const html = new Oak.Router();
+const page = new Oak.Router();
+const dist = new Oak.Router();
 const auth = new Oak.Router();
 const task = new Oak.Router();
 const user = new Oak.Router();
@@ -11,22 +12,28 @@ const user = new Oak.Router();
 const pages = `${Deno.cwd()}/core/pages`;
 const media = `${Deno.cwd()}/core/static`;
 
-const landing = { root: pages, path: "index.html" };
-const login   = { root: pages, path: "login.html" };
-const signup  = { root: pages, path: "signup.html" };
-const lister  = { root: pages, path: "lister.html" };
-const maker   = { root: pages, path: "maker.html" };
-const logo    = { root: media, path: "gear-logo.png" };
-const favicon = { root: media, path: "gear-icon.ico" };
+const page_land   = { root: pages, path: "index.html" };
+const page_login  = { root: pages, path: "login.html" };
+const page_signup = { root: pages, path: "signup.html" };
+const page_lister = { root: pages, path: "lister.html" };
+const page_maker  = { root: pages, path: "maker.html" };
+const logo     = { root: media, path: "gear-logo.png" };
+const favicon  = { root: media, path: "gear-icon.ico" };
 
-html
-	.get("/",       async context => await context.send(landing))
-	.get("/login",  async context => await context.send(login))
-	.get("/signup", async context => await context.send(signup))
-	.get("/lister", async context => await context.send(lister))
-	.get("/maker",  async context => await context.send(maker))
+page
+	.get("/",       async context => await context.send(page_land))
+	.get("/login",  async context => await context.send(page_login))
+	.get("/signup", async context => await context.send(page_signup))
+	.get("/lister", async context => await context.send(page_lister))
+	.get("/maker",  async context => await context.send(page_maker))
 	.get("/logo",   async context => await context.send(logo))
 	.get("/fav",    async context => await context.send(favicon))
+
+dist
+	.get("/login",  async context => await context.send(login_form))
+	.get("/signup", async context => await context.send(signup_form))
+	.get("/lister", async context => await context.send(lister_app))
+	.get("/maker",  async context => await context.send(maker_app))
 
 auth
 	.post("/login",  async context => await Auth.login(context))
@@ -49,7 +56,7 @@ task
 	.delete("/task/:id", async context => await Crud.task_delete(context))
 
 app
-	.use("",          html.routes(), html.allowedMethods())
+	.use("",          page.routes(), page.allowedMethods())
 	.use("/api/auth", auth.routes(), auth.allowedMethods())
 	.use("/api/crud", user.routes(), user.allowedMethods())
 	.use("/api/crud", task.routes(), task.allowedMethods())
